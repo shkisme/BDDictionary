4437554932c2e0fa914dc977343633a27b81554c
shkisme <shkisme0130@gmail.com>
2024-02-19 21:19:02 +0900

    init: 프로젝트 세팅

---
+++ src/test/java/dictionary/ApplicationTest.java
@@ -0,0 +1,17 @@
+package dictionary;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ApplicationTest {
+
+    @Test
+    @DisplayName("test")
+    public void test() {
+        //given
+
+        //when
+
+        //then
+    }
+}
